f133a61a8e3adb9a9d3a1b982dbc02e268e953c8
杜振训 <5552123@qq.com>
2019-12-06 12:57:50 +0800

    websocket

---
+++ demo/webSocket2/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"go-demo/demo/webSocket2/impl"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	addr    = flag.String("addr", ":8080", "register address")
+	content = make([]string, 0) //收到的内容
+	room    string              //房间号
+	mu      sync.Mutex
+)
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/api", apiHandler)
+	http.ListenAndServe(*addr, nil)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// 完成ws协议的握手操作
+	wsConnect, err := upgrader.Upgrade(w, r, nil);
+	if err != nil {
+		return
+	}
+	wsConn, err := impl.InitConnection(wsConnect)
+	if err != nil {
+		wsConn.Close()
+	}
+	for {
+		//接收用户发来信息，回复信息
+		/*data, err := wsConn.ReadMessage()
+		if err != nil {
+			wsConn.Close()
+		}
+		fmt.Println(data)*/
+		//wsConn.WriteMessage(data)
+
+		//fmt.Println(len(content))
+		if len(content) > 0 {
+			mu.Lock()
+			var msg = content
+			content = make([]string, 0)
+			mu.Unlock()
+			for _, v := range msg {
+				time.Sleep(1*time.Second)
+				if err := wsConn.WriteMessage([]byte(v)); err != nil {
+					wsConn.Close()
+				}
+			}
+		} else {
+			//wsConn.WriteMessage([]byte("还没有信息"))
+			time.Sleep(1*time.Second)
+		}
+	}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	room = r.PostFormValue("room")
+	content = append(content, r.PostFormValue("content"))
+	if len(content) > 5 {
+		content = content[:5]
+	}
+	mu.Unlock()
+	result := map[string]string{
+		"code": "1", "msg": fmt.Sprintf("ok,当前信息数量:%v",len(content)),
+	}
+	res, _ := json.Marshal(result)
+	w.Write(res)
+}
--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/protobuf v1.3.2
+	github.com/gorilla/websocket v1.4.1
 	github.com/hashicorp/consul/api v1.2.0
 	github.com/jackdanger/collectlinks v0.0.0-20160421202702-24c4ee2870ba
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c
 	google.golang.org/grpc v1.23.1
 )
